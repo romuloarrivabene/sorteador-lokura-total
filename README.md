42b921679ac6703b5fdf4bb4eadff1606ca5bc60
romuloarrivabene <124391988+romuloarrivabene@users.noreply.github.com>
2023-02-03 15:22:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sorteador-lokura-total
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sorteador-pardinho-gngtnz)
---
+++ src/app/cdk-drag-drop-connected-sorting-group-example.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
+/**
+ * @title Drag&Drop connected sorting group
+ */
+@Component({
+  selector: 'cdk-drag-drop-connected-sorting-group-example',
+  templateUrl: 'cdk-drag-drop-connected-sorting-group-example.html',
+  styleUrls: ['cdk-drag-drop-connected-sorting-group-example.css'],
+})
+export class CdkDragDropConnectedSortingGroupExample {
+  done = [
+    'Mateus',
+    'Rômulo',
+    'Andressa',
+    'Léo',
+    'Camori',
+    'Leandro',
+    'Iago',
+  ];
+
+  todo: string[] = [];
+  finish: string[] = [];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  sorted = 'Sortear';
+  random: number;
+  sortead() {
+    if (this.done.length == 0) return;
+    this.random = this.randomNumber(0, this.done.length - 1);
+    this.sorted = this.done[this.random];
+    this.finish.push(this.sorted);
+    delete this.done[this.random];
+    this.done = this.done.filter((e) => e);
+    console.log(this.done);
+  }
+
+  randomNumber(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
